8badfc4812e99a380d16a42f922edccad43b131b
Donald Brooks Arum <brooksd079@gmail.com>
2022-07-26 22:59:13 +0300

    renders the main and header component with initial text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Main from './components/Main';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
 import './App.css';
 
 function App() {
---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataArea from './DataArea';
+
+const Main = () => {
+  return (
+    <div>
+        <DataArea />
+    </div>
+  )
+}
+
+export default Main;
